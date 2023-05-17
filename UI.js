b9aa70934b1d9ac97ccdab61a4c8096efd81b180
unnxt30 <105226341+unnxt30@users.noreply.github.com>
2023-05-17 12:44:29 +0530

    Updating the code

---
+++ UI.js
@@ -0,0 +1,10 @@
+//UI
+const body = document.querySelector("body");
+
+body.style["backgroundColor"] = "#F8B195";
+
+const textDiv = document.getElementById("greeting");
+
+textDiv.setAttribute("class", "typing")
+
+// textDiv.appendChild(welcomeText);
--- app.js
+++ app.js
@@ -1,53 +1,51 @@
 
-const choices = ["Rock", "Paper", "Scissor"];
-
-function getComputerChoice(choices)
-{
-    let index = Math.floor(Math.random() * choices.length);
-    return choices[index];
-}
-
-// console.log(getComputerChoice(choices));
-let playerSelection;
-let computerSelection;
-
-function getChoice()
-{
-     let playerSelection = prompt("Enter the Player choice: ", "Rock").toUpperCase();
-     let computerSelection = getComputerChoice(choices).toUpperCase(); 
-     selections = [playerSelection, computerSelection];
-     return selections;
-}
-
-// selections = getChoice();
-// console.log(`The Player selected: ${selections[0]}\nThe Computer selected: ${selections[1]} `);
-
-function playRound(pS, cS)
-{   
-    if(pS == "ROCK" && cS == "PAPER")
-    {
-        return "You Lose, Paper beats Rock";
-    }
-
-    else if(pS == "SCISSOR" && cS == "ROCK")
-    {
-        return "You Lose, Rock beats Scissor";
-    }
-    else if(pS == "PAPER" && cS == "SCISSOR")
-    {
-        return "You Lose, Scissor beats Paper";
-    }
-}
-
-function game(n)
-{
-    for(let i = 0; i<n; i++)
-    {   
-        selections = getChoice();
-        console.log(`The Player selected: ${selections[0]}\nThe Computer selected: ${selections[1]} `);
-        console.log(playRound(selections[0], selections[1]));
-    }
-}
 
 
-game(5);    
+// const choices = ["Rock", "Paper", "Scissor"];
+
+// function getComputerChoice(choices)
+// {
+//     let index = Math.floor(Math.random() * choices.length);
+//     return choices[index];
+// }
+
+// let playerSelection;
+// let computerSelection;
+
+// function getChoice()
+// {
+//      let playerSelection = prompt("Enter the Player choice: ", "Rock").toUpperCase();
+//      let computerSelection = getComputerChoice(choices).toUpperCase(); 
+//      selections = [playerSelection, computerSelection];
+//      return selections;
+// }
+
+// function playRound(pS, cS)
+// {   
+//     if(pS == "ROCK" && cS == "PAPER")
+//     {
+//         return "You Lose, Paper beats Rock";
+//     }
+
+//     else if(pS == "SCISSOR" && cS == "ROCK")
+//     {
+//         return "You Lose, Rock beats Scissor";
+//     }
+//     else if(pS == "PAPER" && cS == "SCISSOR")
+//     {
+//         return "You Lose, Scissor beats Paper";
+//     }
+// }
+
+// // function game(n)
+// // {
+// //     for(let i = 0; i<n; i++)
+// //     {   
+// //         selections = getChoice();
+// //         console.log(`The Player selected: ${selections[0]}\nThe Computer selected: ${selections[1]} `);
+// //         console.log(playRound(selections[0], selections[1]));
+// //     }
+// // }
+
+
+// // game(5);    
