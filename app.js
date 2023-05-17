6156d82f63125d549673bfcb9ecf027c1b342242
unnxt30 <105226341+unnxt30@users.noreply.github.com>
2023-05-17 19:59:41 +0530

    Added ui, now working on mechanism

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
 
 
-// const choices = ["Rock", "Paper", "Scissor"];
+const choices = ["Rock", "Paper", "Scissor"];
 
-// function getComputerChoice(choices)
-// {
-//     let index = Math.floor(Math.random() * choices.length);
-//     return choices[index];
-// }
+function getComputerChoice(choices)
+{
+    let index = Math.floor(Math.random() * choices.length);
+    return choices[index];
+}
 
-// let playerSelection;
-// let computerSelection;
+let playerSelection;
+let computerSelection;
 
 // function getChoice()
 // {
@@ -20,32 +20,65 @@
 //      return selections;
 // }
 
-// function playRound(pS, cS)
-// {   
-//     if(pS == "ROCK" && cS == "PAPER")
-//     {
-//         return "You Lose, Paper beats Rock";
-//     }
+function playRound(pS, cS)
+{   
+    if(pS == "ROCK" && cS == "PAPER")
+    {
+        return "You Lose, Paper beats Rock";
+    }
 
-//     else if(pS == "SCISSOR" && cS == "ROCK")
-//     {
-//         return "You Lose, Rock beats Scissor";
-//     }
-//     else if(pS == "PAPER" && cS == "SCISSOR")
-//     {
-//         return "You Lose, Scissor beats Paper";
+    else if(pS == "SCISSOR" && cS == "ROCK")
+    {
+        return "You Lose, Rock beats Scissor";
+    }
+    else if(pS == "PAPER" && cS == "SCISSOR")
+    {
+        return "You Lose, Scissor beats Paper";
+    }
+    else{
+        return "Draw :)"
+    }
+}
+
+// function game(n)
+// {
+//     for(let i = 0; i<n; i++)
+//     {   
+//         selections = getChoice();
+//         console.log(`The Player selected: ${selections[0]}\nThe Computer selected: ${selections[1]} `);
+//         console.log(playRound(selections[0], selections[1]));
 //     }
 // }
 
-// // function game(n)
-// // {
-// //     for(let i = 0; i<n; i++)
-// //     {   
-// //         selections = getChoice();
-// //         console.log(`The Player selected: ${selections[0]}\nThe Computer selected: ${selections[1]} `);
-// //         console.log(playRound(selections[0], selections[1]));
-// //     }
-// // }
+
+// game(5);    
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissor = document.getElementById("scissors");
+
+const result = document.getElementById("result");
 
 
-// // game(5);    
+rock.addEventListener("click", function(){
+    playerSelection = "ROCK";
+    computerSelection = getComputerChoice(choices).toUpperCase();
+    console.log(`${playerSelection} ${computerSelection}`);
+})
+
+paper.addEventListener("click", function(){
+    playerSelection = "PAPER";
+    computerSelection = getComputerChoice(choices).toUpperCase();
+    
+})
+
+scissor.addEventListener("click", function(){
+    playerSelection = "SCISSOR";
+    computerSelection = getComputerChoice(choices).toUpperCase();
+    // console.log(playRound(playerSelection, computerSelection));
+    console.log(`${playerSelection} ${computerSelection}`);
+})
+
+
+
+
+
